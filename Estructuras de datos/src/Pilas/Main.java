945ba57a91a673d8ef0111ca7cde8ae61444ebe6
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-06-12 19:17:11 -0700

    manejo de pila con objeto Alumno

--- Estructuras de datos/src/Pilas/Main.java
+++ Estructuras de datos/src/Pilas/Main.java
@@ -1,22 +1,22 @@
 package Pilas;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        int[] temp = new int[3];
-        int[] temp2 = new int[2];
-        temp[0] = 1;
-        temp[1] = 1;
-        //temp[2] = 1;
 
-        temp2[0] = 2;
-        temp2[1] = 2;
-        System.out.println(temp.length);
+        Alumno alum1 = new Alumno();
+        Alumno alum2 = new Alumno("Alberto", 21);
+        Alumno alum3 = new Alumno("Ramses", 21);
 
-        temp = temp2;
+        Stack<Alumno> pila = new Stack<Alumno>();
 
-        System.out.println(Arrays.toString(temp));
-        System.out.println(temp.length);
+        pila.push(alum1);
+        pila.push(alum2);
+        pila.push(alum3);
+
+        System.out.println(pila.peek().getNombre());
+        System.out.println(pila.peek().getEdad());
     }
 }
