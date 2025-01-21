2940e156cc175f2f116fdcf3ffe957db3b51cb73
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2025-01-20 22:49:06 -0700

    spring - suma

--- Spring Boot/holaMundo/src/main/java/com/example/holaMundo/Controller/HolaMundoController.java
+++ Spring Boot/holaMundo/src/main/java/com/example/holaMundo/Controller/HolaMundoController.java
@@ -14,5 +14,15 @@ public class HolaMundoController {
     }
 
 
-   
+    @GetMapping("/saludo/{miNombre}")
+    public String saludoPersonalizado(@PathVariable("miNombre") String nombre) {
+        return "Hola " + nombre;
+    }
+
+
+    @GetMapping("/suma/{num1}+{num2}")
+    public String suma(@PathVariable("num1") int num1, @PathVariable("num2") int num2) {
+        num1 += num2;
+        return "La suma es: " + num1;
+    }
 }
